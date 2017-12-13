7806d83976963148a20368c18286608cc2d836e1
Loïc Cordone <loic.cordone@gmail.com>
2017-12-13 03:16:39 +0100

    Squelette SpringBoot en cours de réalisation

---
+++ src/main/java/fr/n7/fut/FutApplication.java
@@ -0,0 +1,12 @@
+package fr.n7.fut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutApplication.class, args);
+	}
+}
