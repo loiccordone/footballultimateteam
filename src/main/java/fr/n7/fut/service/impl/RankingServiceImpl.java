d9402fa6f7986751f8b5016da566a4313b636916
BoukraichiHamza <h.boukraichi@gmail.com>
2018-01-12 12:51:54 +0100

    Maj Service

---
+++ src/main/java/fr/n7/fut/service/impl/RankingServiceImpl.java
@@ -0,0 +1,17 @@
+package fr.n7.fut.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import fr.n7.fut.model.players.Player;
+import fr.n7.fut.service.RankingService;
+
+public class RankingServiceImpl implements RankingService {
+
+	@Override
+	public Map<Integer, List<Player>> computeRanking() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
